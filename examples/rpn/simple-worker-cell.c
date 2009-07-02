5bef563538233ba609af586aedaf3e4a80fe6a11
Henri-Pierre Charles <hpc@prism.uvsq.fr>
2009-07-02 18:52:54 +0200

    * Simple worker with limited number of iteration

--- examples/rpn/simple-worker-cell.c
+++ examples/rpn/simple-worker-cell.c
@@ -13,14 +13,15 @@ int main(unsigned long long speid,
 {
    char *functionBuffer;
    int mask = 2;
-   int res, a, i;
+   int res, a, i, limit;
 
    functionBuffer = _malloc_align(SIZE,7); 	/* Alloc memory for function */
    mfc_get(functionBuffer, argp, SIZE, 1, 0, 0);/* Read binary code from ppc */
    mfc_write_tag_mask(mask); 
    mfc_read_tag_status_all();
 
-   while (1)
+   limit = spu_read_in_mbox(); 
+   for (i = 0; i < limit; ++i)
      {
        a = spu_read_in_mbox();   		/* Read parameter */
        res = ((fadd_ptr)functionBuffer)(a);	/* Run code */
