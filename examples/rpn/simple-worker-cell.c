f0760f6db55c305dbcc451c7ba53f46f472d69c6
Henri-Pierre Charles <hpc@prism.uvsq.fr>
2009-11-17 16:13:06 +0100

    Add tick mesuments tested on ps3/x86/itanium2

--- examples/rpn/simple-worker-cell.c
+++ examples/rpn/simple-worker-cell.c
@@ -1,6 +1,5 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <malloc_align.h>
-#include <free_align.h>
 #include <spu_mfcio.h>
 
 #define SIZE 1024		/* Binary code size */
@@ -15,18 +14,20 @@ int main(unsigned long long speid,
    int mask = 2;
    int res, a, i, limit;
 
-   functionBuffer = _malloc_align(SIZE,7); 	/* Alloc memory for function */
+   functionBuffer = memalign(16, SIZE); 	/* Alloc memory for function */
    mfc_get(functionBuffer, argp, SIZE, 1, 0, 0);/* Read binary code from ppc */
    mfc_write_tag_mask(mask); 
    mfc_read_tag_status_all();
 
    limit = spu_read_in_mbox(); 
+   printf("Read limit %d\n", limit);
    for (i = 0; i < limit; ++i)
      {
        a = spu_read_in_mbox();   		/* Read parameter */
+       printf("Read value %d\n", a);
        res = ((fadd_ptr)functionBuffer)(a);	/* Run code */
        spu_write_out_intr_mbox(res);		/* Send result */
      }
-   _free_align(functionBuffer);
+   free(functionBuffer);
    return 0;
 }
