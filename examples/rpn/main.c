294a317b08a299c5b799bf89a6bc693a4b80cee4
Henri-Pierre Charles <hpc@prism.uvsq.fr>
2010-02-10 18:47:26 +0100

    Add PDF documentation and improve examples

--- examples/rpn/main.c
+++ examples/rpn/main.c
@@ -61,6 +61,7 @@ int main(int argc, char * argv[])
       pifi c2f = rpnCompile("9*5/32+");
       pifi f2c = rpnCompile("32-5*9/");
       printf("Code gen %llu ticks \n", getTick() - startTick);
+
       printf("\nC:");
       for (i = 0; i <= 100; i+= 10) printf("%3d ", i);
       printf("\nF:");
@@ -71,14 +72,17 @@ int main(int argc, char * argv[])
       printf("\nC:");
       for (i = 32; i <= 212; i+= 10) printf("%3d ", f2c(i));
       printf("\n");
-      startTick = getTick();
-      for (i = 0; i  <= 100; i+= 10)  	c2f(i);
-      for (i = 32; i <= 212; i+= 10) 	f2c(i);
-      printf("%llu ticks compiled\n", getTick() - startTick);
+
       startTick = getTick();
       for (i = 0; i  <= 100; i+= 10)  	rpnEval("9*5/32+", i);
       for (i = 32; i <= 212; i+= 10) 	rpnEval("32-5*9/", i);
       printf("%llu ticks interpreted\n", getTick() - startTick);
+
+      startTick = getTick();
+      for (i = 0; i  <= 100; i+= 10)  	c2f(i);
+      for (i = 32; i <= 212; i+= 10) 	f2c(i);
+      printf("%llu ticks dynamycally compiled\n", getTick() - startTick);
+
       startTick = getTick();
       for (i = 0; i  <= 100; i+= 10)  	static_c2f(i);
       for (i = 32; i <= 212; i+= 10) 	static_f2c(i);
@@ -87,9 +91,9 @@ int main(int argc, char * argv[])
   else
     {
       pifi fun = rpnCompile (argv[1]);
-      printf("\nC:");
+      printf("\n:");
       for (i = 0; i <= 100; i+= 10) printf("%4d ", i);
-      printf("\nF:");
+      printf("\n:");
       for (i = 0; i <= 100; i+= 10) printf("%4d ", fun(i));
       printf("\n");
     }
